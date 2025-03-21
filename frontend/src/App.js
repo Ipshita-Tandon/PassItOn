3a70f28cbf90b8256e6b5d2dc65434703ffb8bad
Ipshita-Tandon <ipshitatandon@gmail.com>
2025-03-22 01:01:52 +0530

    Added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-import Explore from "./pages/explore.jsx"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/explore.jsx";
 import HomePage from "./pages/homepage.jsx";
 import Popular from "./pages/popular.jsx";
 
 function App() {
   return (
-    <div>
-      <HomePage />
-      <Explore />
-      <Popular />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/popular" element={<Popular />} />
+      </Routes>
+    </Router>
   );
 }
 
