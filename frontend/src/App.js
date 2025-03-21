2a6130c96a9dbbe23e92f2af3db57939a2a50f41
Ipshita-Tandon <ipshitatandon@gmail.com>
2025-03-21 10:32:16 +0530

    Moved to frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HomePage from "./pages/homepage.jsx"; // Import HomePage component
+// import Teammates from "./pages/teammates.jsx";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+      {/* <Teammates/> */}
+    </div>
+  );
+}
+
+export default App;
