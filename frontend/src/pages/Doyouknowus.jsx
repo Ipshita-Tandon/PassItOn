ef0caf0a00beccd8cc398886b35c79bc2222bffa
Bhuvi Singh <bhuvi2singh@gmail.com>
2025-04-28 00:49:51 +0530

    text

--- frontend/src/pages/Doyouknowus.jsx
+++ frontend/src/pages/Doyouknowus.jsx
@@ -23,12 +23,12 @@ const Doyouknowus = () => {
         <div className="left-section">
           <h1 className="heading">Did you know us?</h1>
           <p className="description">
-            We are about books and our purpose is to show you the book that can
-            change your life or transport you to a better world. BWorld
-            collaborates with the most popular publishers just for your delight.
+          Welcome to your one-stop destination for all things educational and inspiring! From textbooks and bestselling novels to everyday stationery and unique finds, our collection is curated to support students, readers, and lifelong learners alike. Whether you're stocking up for a new semester or just looking for your next great read, you’ll find quality items at great prices — all with the trust and care of the PassItOn spirit.
+
+
           </p>
           <p className="subscription-message">
-            If you love books, you must subscribe to our newsletter.
+          For book lovers and savvy shoppers alike, subscribe to our newsletter for exclusive deals and new arrivals!
           </p>
 
           <form onSubmit={handleSubmit} className="subscription-form">
